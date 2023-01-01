8700a311d659ede41cf1d75afd6af3e011b39d22
Hellen Kokach <42536943+hellen17@users.noreply.github.com>
2023-01-01 23:04:16 +0300

    first commit

---
+++ src/components/Heroshot.js
@@ -0,0 +1,36 @@
+import Button from '@mui/material/Button';
+import PersonIcon from '@mui/icons-material/Person';
+import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
+
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles({
+    outlinedButton: {
+        '&.MuiButton-outlined': {
+            borderColor: 'var(--primary-color)',
+            color: 'var(--primary-color)'
+    }}
+})
+
+
+export default function Heroshot(){
+    const classes = useStyles();
+
+    return(
+        <section className="flex px-5">
+            <div className="lg:w-2/3 w-full">
+                <div className="text-left lg:p-32 p-8">
+                    <h1 className="text-4xl font-bold pb-5">Hello, i’m <br></br> Jane Doe</h1>
+                    <p className="text-2xl">Freelance UI designer, Fullstack developer, & Data Miner. I create seamless web experiences for end-users.</p>
+                    <div className="flex mt-5">
+                    <Button variant="contained" style={{ textTransform: 'none', marginRight: '1rem', backgroundColor: 'var(--primary-color)' }} startIcon={<PersonIcon />} >About Me</Button>
+                    <Button variant="outlined" style={{ textTransform: 'none' }} endIcon={<RemoveRedEyeIcon style={{ color: 'white' }} />}  className={classes.outlinedButton} >Projects</Button>
+                    </div>
+                </div>
+            </div>
+            <div className="lg:w-1/3 w-full mx-auto">
+            </div>
+          
+        </section>
+    )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,39 @@
+export function Navbar() {
+    const styles = {
+        nav: {
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            padding: '0 2rem',
+            color: 'white',
+            height: '4rem',
+            
+        },
+        a: {
+            textDecoration: 'none',
+            color: 'white',
+            margin: '0 1rem',
+        },
+
+        logoContainer: {
+            display: 'flex',
+            alignItems: 'center'
+        }
+    }
+
+    return (
+        <nav style={styles.nav}>
+            <div style={styles.logoContainer}> 
+            <p style={{ color: 'var(--primary-color)' }}>Hellen Kokach</p>
+            </div>
+            <div style={{ display: 'flex' }}>
+                <a style={styles.a} href="/">Home</a>
+                <a style={styles.a} href="">About me</a>
+                <a style={styles.a} href="">Projects</a>
+                <a style={styles.a} href="">Contact</a>
+            </div>
+        
+        </nav>
+    )
+}
