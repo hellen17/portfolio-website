9924a864424b4196dd894865328b0f3f982ca285
Hellen Kokach <42536943+hellen17@users.noreply.github.com>
2023-01-16 10:41:38 +0300

    add google analytics

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Analytics from 'react-analytics';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Analytics id="G-X6VQ23D3DS">
     <App />
+    </Analytics>
   </React.StrictMode>
 );
 
