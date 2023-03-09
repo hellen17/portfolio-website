41ce95ee307ea2516bcc34c5413ebae2b917a390
Hellen Kokach <42536943+hellen17@users.noreply.github.com>
2023-03-09 23:15:02 +0300

    add google tag manager

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Contact from './components/Contact';
 import Articles from './components/Articles';
 import Footer from './components/Footer';
 
-const trackingId = "G-9JKM67H3QE"; // Replace with your Google Analytics tracking ID
+const trackingId = "G-7MB83S19P4"; // Replace with your Google Analytics tracking ID
 
 function App() {
 
