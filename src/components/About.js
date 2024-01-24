e2cfec89238771d81583296866aa7064b4d20302
Hellen Kokach <42536943+hellen17@users.noreply.github.com>
2024-01-24 15:40:35 +0300

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -20,10 +20,10 @@ export default function About() {
     return(
         <div className="lg:px-32 p-8" id='about'>
             <h2 className="text-2xl font-bold mb-5" style={{color:'var(--primary-color)'}}>About me:</h2>
-            <p>I am a web developer with over two years of experience building SaaS products in the startup space. I am passionate about innovation and creating solutions that are accessible and user-centric. My technical skills include Django, Python, ReactJS, NodeJS, HTML5, CSS, JavaScript, Tailwind, UI/UX, WordPress & SEO.  
+            <p>I am a web developer with over three years of experience building SaaS products in the startup space. I am passionate about innovation and creating solutions that are accessible and user-centric. My technical skills include Django, Python, ReactJS, NodeJS, HTML5, CSS, JavaScript, Tailwind, UI/UX, WordPress & SEO.  
             </p>
             <div className="flex lg:flex-row flex-col lg:items-center mt-5">
-                <p className="text-8xl font-bold" style={{color:'var(--primary-color)'}}>2+</p>
+                <p className="text-8xl font-bold" style={{color:'var(--primary-color)'}}>3+</p>
                 <p className="lg:ml-8">Years of experience. Specialised in building web apps, while ensuring a seamless
                     web experience for end users.</p>
 
